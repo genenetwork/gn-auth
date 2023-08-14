2039a555704183b737d9363ec60e9dfc78796dc5
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2023-08-14 10:56:38 +0300

    Wrap package definition in `(define-public ...)`

--- .guix/modules/gn/packages/gn-auth.scm
+++ .guix/modules/gn/packages/gn-auth.scm
@@ -104,40 +104,43 @@ clients. It is designed from low level specifications implementations to high
 level frameworks integrations, to meet the needs of everyone.")
    (license license:bsd-3)))
 
-(package
- (name "genenetwork-auth")
- (version (string-append (get-latest-version)
-			 "-git-"
-			 (substring (get-commit) 0 9)))
- (source (local-file %source-dir "genenetwork-auth-checkout"
-		     #:recursive? #t
-		     #:select? vcs-file?))
- (build-system python-build-system)
- (arguments
-  (list
-   #:phases
-   #~(modify-phases %standard-phases
-	(replace 'check
-	  (lambda _ (invoke "pytest" "-k" "unit_test"))))))
- ;; (inputs (list))
- (native-inputs
-  (list python-mypy
-	python-pytest
-	python-pylint
-	python-hypothesis
-	python-pytest-mock
-        python-mypy-extensions))
- (propagated-inputs
-  (list python-flask
-	python-redis
-	python-authlib
-	python-pymonad
-	yoyo-migrations
-	python-bcrypt ;; remove after removing all references
-	python-mysqlclient
-	python-argon2-cffi
-	python-email-validator))
- (home-page "https://github.com/genenetwork/gn-auth")
- (synopsis "Authentication and Authorisation server for GeneNetwork services.")
- (description "Authentication and Authorisation server for GeneNetwork services.")
- (license license:agpl3+))
+(define-public gn-auth
+  (package
+   (name "gn-auth")
+   (version (string-append (get-latest-version)
+			   "-git-"
+			   (substring (get-commit) 0 9)))
+   (source (local-file %source-dir "gn-auth-checkout"
+		       #:recursive? #t
+		       #:select? vcs-file?))
+   (build-system python-build-system)
+   (arguments
+    (list
+     #:phases
+     #~(modify-phases %standard-phases
+		      (replace 'check
+			       (lambda _ (invoke "pytest" "-k" "unit_test"))))))
+   ;; (inputs (list))
+   (native-inputs
+    (list python-mypy
+	  python-pytest
+	  python-pylint
+	  python-hypothesis
+	  python-pytest-mock
+          python-mypy-extensions))
+   (propagated-inputs
+    (list python-flask
+	  python-redis
+	  python-authlib
+	  python-pymonad
+	  yoyo-migrations
+	  python-bcrypt ;; remove after removing all references
+	  python-mysqlclient
+	  python-argon2-cffi
+	  python-email-validator))
+   (home-page "https://github.com/genenetwork/gn-auth")
+   (synopsis "Authentication and Authorisation server for GeneNetwork services.")
+   (description "Authentication and Authorisation server for GeneNetwork services.")
+   (license license:agpl3+)))
+
+gn-auth
