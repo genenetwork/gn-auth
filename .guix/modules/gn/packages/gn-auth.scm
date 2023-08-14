41e131eb8571d0172ee8fdcd659cb93b3f33e89f
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2023-08-14 10:19:33 +0300

    Add missing dependency
    Define `python-authlib` package.

--- .guix/modules/gn/packages/gn-auth.scm
+++ .guix/modules/gn/packages/gn-auth.scm
@@ -1,5 +1,7 @@
 (define-module (gn gn-auth)
   #:use-module (guix)
+  #:use-module (guix packages)
+  #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix build-system python)
   #:use-module ((guix licenses) #:prefix license:)
@@ -21,9 +23,7 @@
 
   #:use-module (gnu packages databases)
 
-
-  ;; Packages from guix-bioinformatics
-  #:use-module (gn packages python-web))
+  #:use-module (gnu packages django))
 
 (define %source-dir (dirname (dirname (dirname (current-filename)))))
 
@@ -74,6 +74,35 @@
 (define vcs-file?
   (or (git-predicate %source-dir)
       (const #t)))
+
+(define-public python-authlib
+  (package
+   (name "python-authlib")
+   (version "1.2.0")
+   (source (origin
+            (method url-fetch)
+            (uri (pypi-uri "Authlib" version))
+            (sha256
+             (base32
+              "178ycfypdv1hy4zjm09rmysxakmwch2n6a5wypwmx4d5hc4fi8sg"))))
+   (build-system python-build-system)
+   (native-inputs (list python-httpx
+			python-flask
+			python-django
+			python-werkzeug
+			python-starlette
+			python-sqlalchemy
+			python-pycryptodomex))
+   (propagated-inputs (list python-cryptography))
+   (arguments `(#:tests? #f)) ;; temporarily deactivate tests
+   (home-page "https://authlib.org/")
+   (synopsis
+    "The ultimate Python library in building OAuth and OpenID Connect servers and clients.")
+   (description
+    "The ultimate Python library in building OAuth and OpenID Connect servers and
+clients. It is designed from low level specifications implementations to high
+level frameworks integrations, to meet the needs of everyone.")
+   (license license:bsd-3)))
 
 (package
  (name "genenetwork-auth")
