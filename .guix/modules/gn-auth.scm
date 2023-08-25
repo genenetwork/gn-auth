a183445c6ecf7e99aee82a2d729378b79bed58e7
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2023-08-25 08:39:29 +0300

    Fix linting issues with package definition.

--- .guix/modules/gn-auth.scm
+++ .guix/modules/gn-auth.scm
@@ -29,41 +29,42 @@
 
 (define-public gn-auth
   (package
-   (name "gn-auth")
-   (version "0.1.0-git")
-   (source (local-file "../.."
-		       "gn-auth-checkout"
-		       #:recursive? #t
-		       #:select? vcs-file?))
-   (build-system python-build-system)
-   (arguments
-    (list
-     #:phases
-     #~(modify-phases %standard-phases
-		      (replace 'check
-			       (lambda _ (invoke "pytest" "-k" "unit_test"))))))
-   ;; (inputs (list))
-   (native-inputs
-    (list python-mypy
-	  python-pytest
-	  python-pylint
-	  python-hypothesis
-	  python-pytest-mock
-          python-mypy-extensions))
-   (propagated-inputs
-    (list gunicorn
-	  python-flask
-	  python-redis
-	  python-authlib
-	  python-pymonad
-	  yoyo-migrations
-	  python-bcrypt ;; remove after removing all references
-	  python-mysqlclient
-	  python-argon2-cffi
-	  python-email-validator))
-   (home-page "https://github.com/genenetwork/gn-auth")
-   (synopsis "Authentication and Authorisation server for GeneNetwork services.")
-   (description "Authentication and Authorisation server for GeneNetwork services.")
-   (license license:agpl3+)))
+    (name "gn-auth")
+    (version "0.1.0-git")
+    (source
+     (local-file "../.."
+		 "gn-auth-checkout"
+		 #:recursive? #t
+		 #:select? vcs-file?))
+    (build-system python-build-system)
+    (arguments
+     `(#:phases
+       #~(modify-phases %standard-phases
+	   (replace 'check
+	     (lambda* (#:key tests? #:allow-other-keys)
+	       (when tests?
+		 (invoke "pytest" "-k" "unit_test")))))))
+    (native-inputs
+     (list python-mypy
+	   python-pytest
+	   python-pylint
+	   python-hypothesis
+	   python-pytest-mock
+           python-mypy-extensions))
+    (propagated-inputs
+     (list gunicorn
+	   python-flask
+	   python-redis
+	   python-authlib
+	   python-pymonad
+	   yoyo-migrations
+	   python-bcrypt ;; remove after removing all references
+	   python-mysqlclient
+	   python-argon2-cffi
+	   python-email-validator))
+    (home-page "https://github.com/genenetwork/gn-auth")
+    (synopsis "Authentication and Authorisation server for GeneNetwork services")
+    (description "Authentication and Authorisation server for GeneNetwork services.")
+    (license license:agpl3+)))
 
 gn-auth
