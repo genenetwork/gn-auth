961bef8934f8f37081214aa3ff1d58766800fe3f
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2023-09-22 07:41:02 +0300

    Add default `REDIS_URI` configuration.

--- gn_auth/settings.py
+++ gn_auth/settings.py
@@ -12,6 +12,9 @@ SQL_URI = "mysql://webqtlout:webqtlout@localhost/db_webqtl"
 AUTH_DB = f"{os.environ.get('HOME')}/genenetwork/gn3_files/db/auth.db"
 AUTH_MIGRATIONS = "migrations/auth"
 
+# Redis settings
+REDIS_URI = "redis://localhost:6379/0"
+
 # OAuth2 settings
 OAUTH2_SCOPE = (
     "profile", "group", "role", "resource", "user", "masquerade",
