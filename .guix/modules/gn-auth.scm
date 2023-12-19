4e407e46ab1db9fdcd18d90c5c0d96eb0ef31ce7
Arun Isaac <arunisaac@systemreboot.net>
2023-12-19 21:22:09 +0000

    guix: Inherit gn-auth package from guix-bioinformatics.
    * .guix/modules/gn-auth.scm (gn-auth): Inherit from gn-auth in
    guix-bioinformatics.

--- .guix/modules/gn-auth.scm
+++ .guix/modules/gn-auth.scm
@@ -1,26 +1,12 @@
 (define-module (gn-auth)
+  #:use-module ((gn packages genenetwork)
+                #:select (gn-auth) #:prefix gn:)
   #:use-module (guix)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
-  #:use-module (guix build-system python)
-  #:use-module ((guix licenses) #:prefix license:)
-
-  ;; Packages from guix
-  #:use-module (gnu packages check)
-
-  #:use-module (gnu packages django)
-
-  #:use-module (gnu packages python-web)
-  #:use-module (gnu packages python-xyz)
-  #:use-module (gnu packages python-check)
-  #:use-module (gnu packages python-crypto)
-
-  #:use-module (gnu packages databases)
-
-  ;; Packages from guix-bioinformatics
-  #:use-module (gn packages python-web))
+  #:use-module ((guix licenses) #:prefix license:))
 
 (define %source-dir (dirname (dirname (current-source-directory))))
 
@@ -30,43 +16,11 @@
 
 (define-public gn-auth
   (package
-    (name "gn-auth")
-    (version "0.1.0-git")
+    (inherit gn:gn-auth)
     (source
      (local-file "../.."
 		 "gn-auth-checkout"
 		 #:recursive? #t
-		 #:select? vcs-file?))
-    (build-system python-build-system)
-    (arguments
-     (list
-      #:phases
-      #~(modify-phases %standard-phases
-	  (replace 'check
-	    (lambda* (#:key tests? #:allow-other-keys)
-	      (when tests?
-		(invoke "pytest" "-k" "unit_test")))))))
-    (native-inputs
-     `(("python-mypy" ,python-mypy)
-       ("python-pytest" ,python-pytest)
-       ("python-pylint" ,python-pylint)
-       ("python-hypothesis" ,python-hypothesis)
-       ("python-pytest-mock" ,python-pytest-mock)
-       ("python-mypy-extensions" ,python-mypy-extensions)))
-    (propagated-inputs
-     `(("gunicorn" ,gunicorn)
-       ("python-flask" ,python-flask)
-       ("python-redis" ,python-redis)
-       ("python-authlib" ,python-authlib)
-       ("python-pymonad" ,python-pymonad)
-       ("yoyo-migrations" ,yoyo-migrations)
-       ("python-flask-cors" ,python-flask-cors)
-       ("python-mysqlclient" ,python-mysqlclient)
-       ("python-argon2-cffi" ,python-argon2-cffi)
-       ("python-email-validator" ,python-email-validator)))
-    (home-page "https://github.com/genenetwork/gn-auth")
-    (synopsis "Authentication and Authorisation server for GeneNetwork services")
-    (description "Authentication and Authorisation server for GeneNetwork services.")
-    (license license:agpl3+)))
+		 #:select? vcs-file?))))
 
 gn-auth
