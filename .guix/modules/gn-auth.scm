549f7e08d4538bb18b06f1601afad5fdc7159d30
Arun Isaac <arunisaac@systemreboot.net>
2023-12-19 21:39:24 +0000

    guix: Add gn-auth-all-tests.
    * .guix/modules/gn-auth.scm: Import python-pylint from (gnu packages
    check) and python-mypy from (gnu packages python-check).
    (gn-auth-all-tests): New variable.

--- .guix/modules/gn-auth.scm
+++ .guix/modules/gn-auth.scm
@@ -1,6 +1,8 @@
 (define-module (gn-auth)
   #:use-module ((gn packages genenetwork)
                 #:select (gn-auth) #:prefix gn:)
+  #:use-module ((gnu packages check) #:select (python-pylint))
+  #:use-module ((gnu packages python-check) #:select (python-mypy))
   #:use-module (guix)
   #:use-module (guix gexp)
   #:use-module (guix packages)
@@ -22,5 +24,24 @@
 		 "gn-auth-checkout"
 		 #:recursive? #t
 		 #:select? vcs-file?))))
+
+(define-public gn-auth-all-tests
+  (package
+    (inherit gn-auth)
+    (arguments
+     (substitute-keyword-arguments (package-arguments gn-auth)
+       ((#:phases phases #~%standard-phases)
+        #~(modify-phases #$phases
+            (add-before 'build 'pylint
+              (lambda _
+                (invoke "pylint" "main.py" "setup.py" "wsgi.py"
+                        "tests" "gn_auth" "scripts")))
+            (add-after 'pylint 'mypy
+              (lambda _
+                (invoke "mypy" ".")))))))
+    (native-inputs
+     (modify-inputs (package-native-inputs gn-auth)
+       (prepend python-mypy)
+       (prepend python-pylint)))))
 
 gn-auth
