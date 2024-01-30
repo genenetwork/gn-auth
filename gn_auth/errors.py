10ec3b1f851897c2cce89356b4404c18f9f11a2f
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2024-01-30 07:27:53 +0300

    Add exception traces to error outputs.

--- gn_auth/errors.py
+++ gn_auth/errors.py
@@ -1,28 +1,37 @@
 """Handle application level errors."""
+import traceback
+
 from werkzeug.exceptions import NotFound
 from flask import Flask, request, jsonify, current_app, render_template
 
 from gn_auth.auth.authorisation.errors import AuthorisationError
 
+def add_trace(exc: Exception, errobj: dict) -> dict:
+    """Add the traceback to the error handling object."""
+    return {
+        **errobj,
+        "error-trace": "".join(traceback.format_exception(exc))
+    }
+
 def page_not_found(exc):
     """404 handler."""
     content_type = request.content_type
     if bool(content_type) and content_type.lower() == "application/json":
-        return jsonify({
+        return jsonify(add_trace(exc, {
             "error": exc.name,
             "error_description": (f"The page '{request.url}' does not exist on "
                                   "this server.")
-        }), 404
+        })), 404
 
     return render_template("404.html", page=request.url)
 
 def handle_authorisation_error(exc: AuthorisationError):
     """Handle AuthorisationError if not handled anywhere else."""
     current_app.logger.error(exc)
-    return jsonify({
+    return jsonify(add_trace(exc, {
         "error": type(exc).__name__,
         "error_description": " :: ".join(exc.args)
-    }), exc.error_code
+    })), exc.error_code
 
 __error_handlers__ = {
     AuthorisationError: handle_authorisation_error,
